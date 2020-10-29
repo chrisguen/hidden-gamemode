922ffef5fd42ce4cd96023b78bdd29ecd8c7fb40
Chriss <c.guennewich@gmail.com>
2020-10-29 01:47:35 +0100

    Spectator test

--- ext/Client/__init__.lua
+++ ext/Client/__init__.lua
@@ -1,4 +1,12 @@
-print("mod loaded")
+
 NetEvents:Subscribe("netMakeSuperSoldier", function()
     Events:Dispatch("customEvent")
 end)
+
+Console:Register('setspec', 'SetSpectating true or false', function(args)
+    if args[1] == true then
+        SpectatorManager:SetSpectating(true)
+        else SpectatorManager:SetSpectating(false)
+    end
+end)
+
